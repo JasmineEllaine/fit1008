1c0967ea7bc49565b8a568c2641472a4b57095ef
Jasmine Banares <jasminebanares@dyn-118-139-54-248.its.monash.edu.au>
2018-08-06 14:48:20 +1000

    progress on task 2

--- 3-lab/task2_a.py
+++ 3-lab/task2_a.py
@@ -6,3 +6,64 @@ while index < listSize:
     array[index] = int(input(msg))
     index += 1
 print("The range of this list (max-min) is", max(array)-min(array))
+
+
+"""
+
+.data
+the_list: .word 0
+num: .word 1
+
+
+.text
+
+# $a0 must hold size * 4 + 4
+addi $t2, $0, 5
+addi $t3, $0, 4
+mult $t3, $t2 #compute 5*4
+mflo $t4 #t4 = 5*4
+addi $t4, $t4, 4 #t4 = 5*4 + 4
+
+#getting the right amount to allocate
+add $a0, $t4, $0 #a0 holds the value of t4 + 0
+addi $v0, $0, 9 #allocate memory syscall
+syscall
+#initiate list
+sw $v0, the_list
+addi $t2, $0, 5 #the size
+lw $t6, the_list
+sw $t2, ($t6)
+
+Loop:
+	lw $t0, num #get num
+	addi $t1, $0, 5 #get the 5
+	addi $t1, $t1, 1 #get 5+1
+	bge $t0, $t1, endLoop #where num >= (5+1) goto endloop
+
+	lw $t6, num
+	addi $t6, $t6, -1 #t6 = num-1
+	
+	#compute the address
+	lw $t7, the_list #the START of the list (where the size)
+	addi $t7, $t7, 4 #shift t7 to be talking about the 0th element in the_list
+	
+	addi $t1, $0, 4 #get 4
+	mult $t1,$t6 #computing (num-1)*4 to give us the offset AFTER size from the address
+	mflo $t1 #t1 holds offset in bytes
+	add $t7, $t7, $t1 #t7 holds the address of the element to store
+
+	lw $t5, num
+	mult $t5, $t5
+	mflo $t5 #t5 = num*num
+
+	sw $t5, ($t7)
+
+	lw $t0, num #reget num
+	addi $t0, $t0, 1 #$t0 = num + 1
+	sw $t0, num #num = num + 1
+	j Loop
+
+endLoop:
+	addi $v0, $0, 10 #halt syscall
+	syscall
+"""
--- 3-lab/task2_c.asm
+++ 3-lab/task2_c.asm
@@ -1,28 +1,114 @@
 .data
-        listSize:   .word   0
-        array:      .space  0
-        index:      .word   0
-        inputlist:  .asciiz "/nEnter list size: "
-        inputelem:  .asciiz "/nInput elem: "
-        rangemsg:   .asciiz "The range of this list (max-min) is: "
+	arrayPrompt:		.asciiz	"\nEnter list size: "
+	arraySize: 		.word	0
+	array: 			.word	0
+	
+	index:			.word	0
+	elemPrompt: 		.asciiz "\nEnter element "
+	colon: 			.asciiz ": "
+	currElem:  		.word	0
+	
+	currMax:		.word 	0
+	currMin:		.word	0
+	range: 			.word	0
+	rangePrint:		.asciiz	"The range of this list (max - min) is: "
+	
 .text
-        li $v0, 4
-        la $a0, inputlist
-        syscall                 # prints prompt (inputList)
+    	li      $v0, 4        			# prints prompt
+    	la      $t0, arrayPrompt 
+    	syscall
 
-        # ask for int input (listSize)
-        li  $v0, 5         		# get size of array
-        syscall
+    	li 	    $v0, 5         		# gets input for size of array
+    	syscall
+    	sw	    $v0, arraySize		# stores input in arraySize
 
+	# memory to allocate = size * 4 + 4
+	addi	$s0, $0, 4			# $s0 = 4
+	mul  	$t0, $s0, $v0 			# compute size * 4	
+	addi 	$s0, $t0, 4			# s0 = size *4 + 4
+	
+	# $a0 must hold size of memory to allocate
+	move 	$a0, $t0			# move $s0 into $a0
+	li	 	$v0, 9 			# allocate memory syscall
+	syscall
 
+	# initiate array
+	sw		$v0, array		# store in array the address of first byte allocated
 
-lw      $t0, size
-addi    $t1, $0, 4
-mult    $t1, $t0
-mflo    $t2
-add     $a0, $t2, $t1   # $a0 = 4*size + 4
-addi    $v0, $0, 9      # $v0 = 9
-syscall                 # allocate memory
+	Loop:
+		lw 	$s0, index 		# $s0 = index
+		addi 	$s1, $0, 5 		# $s1 = arraySize
+		bge 	$s0, $s1, endLoop	# if index >= arraySize goto endloop
 
-sw      $v0, the_list   # the_list now points to the returned address
-sw      $t0, ($v0)
+		# compute the address to store word
+		lw 	$s2, array 		# $s2 = the START of the list (where the size)
+		addi 	$s2, $s2, 4 		# shift s2 to be talking about the 0th element in array
+		
+		addi 	$t0, $0, 4 		# $t0 = 4
+		mul 	$t1, $t0, $s0 		# computing (index)*4 to give us the offset AFTER size from the address
+						# t1 holds offset in bytes
+		add 	$s2, $s2, $t1 		# s2 holds the address of the element to store
+
+		# get element to store
+		li      $v0, 4          	# prints elemPrompt
+		la      $a0, elemPrompt
+		syscall
+
+		li	$v0, 1			# prints index
+		lw	$a0, index
+		syscall 
+
+		li      $v0, 4          	# prints colon
+		la      $a0, colon
+		syscall
+
+		li 	$v0, 5          	# gets input element for current index
+		syscall
+		sw	$v0, currElem		# stores input in currElem
+		
+		lw	$s3, currElem		# gets currElem
+		sw	$s3, ($s2)		# stores currElem in array
+
+		# finding range of array
+		bne	$0, $s0, findMax	# if not first index, compare to current max/min
+		sw	$s3, currMax		# else set first elem to max/min
+		sw	$s3, currMin
+
+		updateIndex:
+			# update index
+			addi	$s0, $s0, 1	# add 1 to index
+			sw	$s0, index	
+		j Loop
+
+	endLoop:
+		# calculating range of array
+		sub	$s6, $s4, $s5		# max - min
+		sw	$s6, range		# store range
+
+		# printing range of array
+		li      $v0, 4          	# prints rangePrint
+		la      $a0, rangePrint
+		syscall
+
+		li	$v0, 1			# prints range
+		lw	$a0, range
+		syscall 
+
+		j exit
+
+	# conditions for finding maximum and minimum
+	findMax:
+		lw	$s4, currMax		# $s4 = current max value
+		ble	$s3, $s4, findMin	# if currElem <= currMax then findMin
+		sw	$s3, currMax		# else set currMax = currElem
+
+	findMin:
+		lw	$s5, currMin		# $s4 = current max value
+		bge	$s3, $s5, updateIndex	# if currElem >= currMin then jump back to loop
+		sw	$s3, currMin		# else set currMax = currElem
+		j updateIndex
+
+	# exit program
+	exit: 
+		addi	$v0, $0, 10 
+		syscall
