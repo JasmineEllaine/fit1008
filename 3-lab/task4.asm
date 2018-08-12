f129c58885789c5272858908b014ac44e12fb034
Jasmine Banares <jasminebanares@Jasmines-MacBook-Pro.local>
2018-08-12 12:43:52 +1000

    documented task 5

--- 3-lab/task4.asm
+++ 3-lab/task4.asm
@@ -10,17 +10,22 @@
 	elemPrompt: 		.asciiz "Enter element "
 	colon: 			.asciiz ": "
 	currElem:  		.word	0
-	
 	currMax:		.word 	0
+
+	bitList:		.word 	0
+	bitListSize:	.word	0
+
+	appears: 		.asciiz " appears "
+	times: 			.asciiz " time(s)\n"
 	
 .text
-    	li      $v0, 4	 			# prints prompt
-    	la      $a0, arrayPrompt 
-    	syscall
+	li      $v0, 4	 			# prints prompt
+	la      $a0, arrayPrompt 
+	syscall
 
-    	li 	    $v0, 5         		# gets input for size of array
-    	syscall
-    	sw	    $v0, arraySize		# stores input in arraySize
+	li 	    $v0, 5         		# gets input for size of array
+	syscall
+	sw	    $v0, arraySize		# stores input in arraySize
 
 	# memory to allocate = size * 4 + 4
 	addi	$s0, $0, 4			# $s0 = 4
@@ -28,7 +33,7 @@
 	addi 	$s0, $t0, 4			# s0 = size *4 + 4
 	
 	# $a0 must hold size of memory to allocate
-	move 	$a0, $t0			# move $s0 into $a0
+	move 	$a0, $s0			# move $s0 into $a0
 	li	$v0, 9 				# allocate memory syscall
 	syscall
 
@@ -87,8 +92,90 @@
             j updateIndex
 
 	endLoop:
+    	# start of tallying frequencies
+		# re-initialise index
+		sw	    $0, index
 
-    # start of tallying frequencies
+	# make bitList
+	# memory to allocate to bitList = (currMax + 1) * 4 + 4
+	addi	$s0, $0, 4			# $s0 = 4
+	lw		$t0, currMax		# $t0 = currMax + 1
+	sw 		$t0, bitListSize
+	lw		$t0, bitListSize
+	addi	$t0, $t0, 1
+	mul  	$t0, $t0, $s0 			# compute (currMax + 1) * 4	
+	addi 	$s0, $t0, 4			# s0 = (currMax * 4) + 4
+	
+	# $a0 must hold size of memory to allocate
+	move 	$a0, $s0			# move $s0 into $a0
+	li	$v0, 9 				# allocate memory syscall
+	syscall
+
+	# initiate bitList
+	sw	$v0, bitList			# store in bitList the address of first byte allocated
+
+	bitLoop:
+		lw 	$s0, index 		# $s0 = index
+		lw 	$s1, arraySize 		# $s1 = arraySize
+		bge 	$s0, $s1, printBitList	# if index >= arraySize goto endloop
+
+		# compute the address to get word
+		lw 	$s2, bitList 		# $s2 = the START of the list (where the size)
+		addi 	$s2, $s2, 4 		# shift s2 to be talking about the 0th element in array
+		
+		addi 	$t0, $0, 4 		# $t0 = 4
+		mul 	$t1, $t0, $s0 		# computing (index)*4 to give us the offset AFTER size from the address
+						# t1 holds offset in bytes
+		add 	$s2, $s2, $t1 		# s2 holds the address of the element to get
+
+		lw		$s3, ($s2)		# $s3 = current temp in array
+		addi	$s5, $0, 5		# $s5 = 4
+		mul		$s4, $s3, $s5	# $s4 = currentTemp * 4 
+		add		$s4, $s2, $s4	# $s4 = address of index of bitList to be updated
+
+		lw		$s5, ($s4)		# $s5 = holds current tally
+		addi	$s5, $s5, 1		# $s5 = holds updated tally
+		sw		$s5, ($s4)		# update bitList tally
+
+		updateIndex:
+			# update index
+			addi	$s0, $s0, 1	# add 1 to index
+			sw	$s0, index	
+		j bitLoop
+
+	# prints temperature frequency neatly
+	printBitList:
+
+
+
+
+
+
+
+
+
+		#print all elements (may succeed with indirect addressing) 
+		lw		$t0, 0($t0) 
+		li		$v0, 1 		# load system call (print integer) into syscall register 
+		move 	$a0, $t0 	# load argument for syscall 
+		syscall 			# print element 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	endPrintBitList
 
 	# exit program
 	exit: 
---
+++ 3-lab/task5.asm
@@ -0,0 +1,66 @@
+# Task 5
+#     Faithful translation of task 1a with functions
+
+.data
+    prompt:     .asciiz "Enter a year: "
+    isLeap:     .asciiz "Is a leap year"
+    isNotLeap:  .asciiz "Is not a leap year"
+    year: 	.word 	0
+
+.text
+    main: 
+        li      $v0, 4          # prints prompt
+        la      $a0, prompt
+        syscall
+        
+        li 	    $v0, 5          # gets integer input
+        syscall
+        sw	    $v0, year       # store input in year
+
+        # prepare to call function isLeapYear
+        lw      $a0, year
+        jal		isLeapYear		# jump to isLeapYear and save position to $ra
+        beq		$v1, $0, printNot	# if false then printNot
+
+        printLeap:
+            li      $v0, 4          # prints leap
+            la      $a0, isLeap
+            syscall
+            j exit
+
+        printNot:
+            li      $v0, 4          # prints not leap
+            la      $a0, isNotLeap
+            syscall
+
+    exit:
+        li      $v0, 10
+        syscall
+
+    isLeapYear:
+    # stores return values in v1
+    #   0 = false, 1 = true
+        li      $t0, 100        # div by 100
+        div     $a0, $t0
+        mfhi    $t1             # t1 = remainder
+        bne     $t1, $0, isDivBy4   # jump to 4 if not true
+
+        li      $t0, 400        # div by 400
+        div     $a0, $t0        # t1 = rem
+        mfhi    $t1
+        beq     $t1, $0, returnTrue  # go to printLeap if div by 400
+        j returnFalse
+
+    isDivBy4:
+        li      $t0, 4          # div by 4
+        div     $a0, $t0
+        mfhi    $t1
+        bne     $t1, $0, returnFalse     
+
+    returnTrue:
+        addi    $v1, $0, 1
+        jr		$ra					# jump to $ra
+
+    returnFalse:
+        addi    $v1, $0, 0
+        jr		$ra					# jump to $ra
