8f501a9b66316ba72655a1c3b549a999ba84ca89
Jasmine Banares <jasminebanares@192-168-1-4.tpgi.com.au>
2018-09-15 18:33:46 +1000

    added task 3

--- 8-lab/3-task.py
+++ 8-lab/3-task.py
@@ -4,10 +4,9 @@
 """
  
 class Node:
-    def __init__(self, value):
+    def __init__(self, value=None, next=None):
         self.value = value
-        self.next = None
-        self.prev = None
+        self.next = next
 
 class LinkedList: 
     def __init__(self, head=None, last=None):
@@ -18,11 +17,34 @@ class LinkedList:
         # checks if linked list is empty
         return (self.head == self.last == None)
 
-
     def __str__(self):
         string = ""
         current = self.head
         if not self.isEmpty():
             while (current.next != None):
                 string += current.value + ", "
-                
+            return string[:-2]
+        return string
+
+    def __len__(self):
+        current = self.head
+        i = 0 if (current.next == None) else (1)
+        while (current.next != None):
+            i += 1
+        return i
+
+    def __contains__(self, item):
+        current = self.head
+        while (current != None):
+            if (current.value == item) and (type(current.value) == type(item)):
+                return True
+            current = current.next
+        return False
+
+    def __getitem__(self, index):
+
+def linkedTestFunc():
+    # initialise list
+    myList = LinkedList()
+
+linkedTestFunc()
