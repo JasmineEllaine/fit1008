e2cd1171eef5a8fb93da9da8d811afad762b8bc8
Jasmine Banares <jasminebanares@192-168-1-9.tpgi.com.au>
2018-10-15 01:01:59 +1100

    coded task 5

--- 11-lab/5-task.py
+++ 11-lab/5-task.py
@@ -1,6 +1,10 @@
 from task3 import hash_table
 from string import punctuation
 import time
+import pickle
+import csv
+# with open(filename, ‘wb’) as f:
+#     pickle.dump(your_content, f)
 
 def import_text(filename):
     text_file = open(filename)
@@ -15,13 +19,25 @@ def import_text(filename):
 def main():
     words_list = import_text('text.txt')
     word_hash_table = hash_table(399989)
+
     t1 = time.time()
-    for word in words_list[:50]:
+    for word in words_list:
         if word not in word_hash_table:
             word_hash_table[word] = 0
         word_hash_table[word] += 1
+    
+    # with open('words-hash-table.pickle', 'wb') as f:
+    #     pickle.dump(word_hash_table, f)
     t2 = time.time()
     print(t2-t1)
+
+    with open('words_frequency_list.csv', mode='w') as frequency_file:
+        frequency_writer = csv.writer(frequency_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        for i in range(word_hash_table.table_size):
+            cur_word = word_hash_table.array[i]
+            if cur_word is not None:
+                frequency_writer.writerow([cur_word.key, cur_word.value])
         
 if __name__ == "__main__":
     main()
